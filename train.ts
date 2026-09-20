9d0506baefb00f838d93991149126fcdd8c5541c
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-20 17:08:29 +0500

    feat: Completed Task L

---
+++ train.ts
@@ -0,0 +1,20 @@
+// Task l
+
+const reverseString = (sentence: string): string => {
+  const words: string[] = sentence.split(" ");
+  const result: string[] = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let reversedWord: string = "";
+
+    for (let j = words[i].length - 1; j >= 0; j--) {
+      reversedWord += words[i][j];
+    }
+
+    result.push(reversedWord);
+  }
+
+  return result.join(" ");
+};
+
+console.log(reverseString("Hello World From Me!"));
