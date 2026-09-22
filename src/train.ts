571cbc853e5f066d9065b689fb9087cacf433b66
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-22 17:03:22 +0500

    feat: Completed Task M

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,20 @@
+// TASK M
+
+function getSquareNumbers(arr: number[]): { number: number; square: number }[] {
+  const result: { number: number; square: number }[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push({
+      number: arr[i],
+      square: arr[i] * arr[i],
+    });
+  }
+
+  return result;
+}
+
+console.log(getSquareNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
 // Task L
 
 // const reverseString = (sentence: string): string => {
